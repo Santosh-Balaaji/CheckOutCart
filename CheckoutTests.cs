eb0309f45e9df1fd2c684a6837bf46eea9ed7aad
Santosh Selvakumar <Santosh.Selvakumar@serko.com>
2019-12-09 10:00:58 +1300

    Clean up

--- CheckoutTests.cs
+++ CheckoutTests.cs
@@ -4,16 +4,11 @@ namespace ShoppingCart_Test
 {
     public class CheckoutTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void CalculateTotal_EmptyCart_Return0()
         {
             var checkout = new Checkout();
-            decimal total = checkout.CalculateTotal();
+            var total = checkout.CalculateTotal();
             Assert.AreEqual(0, total);
         }
     }
